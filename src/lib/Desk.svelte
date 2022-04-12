6e9ec1d2258bd7fac98681114b1dfdf9dbabf510
Steven Duzevich <u5824521@anu.edu.au>
2022-04-12 18:04:37 +0100

    background set
    need to fix padding on left?!

--- src/lib/Desk.svelte
+++ src/lib/Desk.svelte
@@ -2,57 +2,12 @@
   import { fade } from 'svelte/transition';
 </script>
 
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
-<div>test</div>
+<div />
 
 <style>
   div {
     background-image: url('../assets/background.png');
     width: 100vw;
-    margin: 0;
-    padding: 0;
+    height: 100vh;
   }
 </style>
